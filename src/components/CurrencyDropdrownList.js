c28bf09f4874d8ba222cb0280384e331ed6df5ba
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-07-19 14:27:24 +0000

    close currency dropdown after currency is selected

--- src/components/CurrencyDropdrownList.js
+++ src/components/CurrencyDropdrownList.js
@@ -2,18 +2,22 @@ import { Component } from "react";
 import styles from "../styles/CurrencyDropdrownList.module.scss";
 
 class CurrencyDropdownList extends Component {
-  render() {
-    return (
-      <div className={styles["dropdown-list"]}>
-        {this.props.list.map(([currency, symbol]) => (
-          <span
-            onClick={() => this.props.select(currency)}
-            key={currency}
-          >{`${symbol} ${currency}`}</span>
-        ))}
-      </div>
-    );
-  }
+	selectHandler(currency) {
+		this.props.select(currency);
+		this.props.close();
+	}
+	render() {
+		return (
+			<div className={styles["dropdown-list"]}>
+				{this.props.list.map(([currency, symbol]) => (
+					<span
+						onClick={() => this.selectHandler(currency)}
+						key={currency}
+					>{`${symbol} ${currency}`}</span>
+				))}
+			</div>
+		);
+	}
 }
 
 export default CurrencyDropdownList;
--- src/components/CurrencyOption.js
+++ src/components/CurrencyOption.js
@@ -5,47 +5,48 @@ import styles from "../styles/CurrencyOption.module.scss";
 import CurrencyDropdownList from "./CurrencyDropdrownList";
 
 const currencyList = new Map([
-  ["USD", "$"],
-  ["EUR", "€"],
-  ["JPY", "¥"],
+	["USD", "$"],
+	["EUR", "€"],
+	["JPY", "¥"],
 ]);
 
 class CurrencyOption extends Component {
-  static contextType = CurrencyContext;
+	static contextType = CurrencyContext;
 
-  state = {
-    currencySymbol: currencyList.get(this.context.currency),
-    showList: false,
-  };
+	state = {
+		currencySymbol: currencyList.get(this.context.currency),
+		showList: false,
+	};
 
-  select(currency) {
-    const symbol = currencyList.get(currency);
-    this.setState({ currencySymbol: symbol });
-    this.context.switchCurrency(currency, symbol);
-  }
+	select(currency) {
+		const symbol = currencyList.get(currency);
+		this.setState({ currencySymbol: symbol });
+		this.context.switchCurrency(currency, symbol);
+	}
 
-  showList = () => {
-    this.setState((prevState) => {
-      return { showList: !prevState.showList };
-    });
-  };
+	showListHandler = () => {
+		this.setState((prevState) => {
+			return { showList: !prevState.showList };
+		});
+	};
 
-  render() {
-    const { currency } = this.context;
-    console.log("CurrenyOption Component", currency);
-    return (
-      <div className={styles["currency-option"]}>
-        <span>{this.state.currencySymbol}</span>
-        <SelectIcon onClick={this.showList.bind(this)} />
-        {this.state.showList && (
-          <CurrencyDropdownList
-            list={Array.from(currencyList)}
-            select={this.select.bind(this)}
-          />
-        )}
-      </div>
-    );
-  }
+	render() {
+		const { currency } = this.context;
+		console.log("CurrenyOption Component", currency);
+		return (
+			<div className={styles["currency-option"]}>
+				<span>{this.state.currencySymbol}</span>
+				<SelectIcon onClick={this.showListHandler.bind(this)} />
+				{this.state.showList && (
+					<CurrencyDropdownList
+						list={Array.from(currencyList)}
+						select={this.select.bind(this)}
+						close={this.showListHandler.bind(this)}
+					/>
+				)}
+			</div>
+		);
+	}
 }
 
 export default CurrencyOption;
