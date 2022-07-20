cfd809fe6a659ed3a75f62f992ad0f38c189db1e
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-07-20 13:51:56 +0000

    using media query for padding

---
+++ src/pages/ProductDescription.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class ProductDescription extends Component {
+	render() {
+		return;
+	}
+}
+
+export default ProductDescription;
--- src/pages/ProductsListing.js
+++ src/pages/ProductsListing.js
@@ -1,44 +1,43 @@
 import { Component } from "react";
 import ProductCard from "../components/ProductCard";
 import ProductList from "../layout/ProductList";
-import styles from "../styles/Products.module.scss";
 
 class ProductsListing extends Component {
-  render() {
-    return (
-      <main className={styles.main}>
-        <h2>women</h2>
-        <ProductList>
-          <>
-            <ProductCard
-              src="https://source.unsplash.com/random/?dress"
-              title="dress"
-              price="200"
-              currency="$"
-            />
-            <ProductCard
-              src="https://source.unsplash.com/random/?dress"
-              title="dress"
-              price="200"
-              currency="$"
-            />
-            <ProductCard
-              src="https://source.unsplash.com/random/?dress"
-              title="dress"
-              price="200"
-              currency="$"
-            />
-            <ProductCard
-              src="https://source.unsplash.com/random/?dress"
-              title="dress"
-              price="200"
-              currency="$"
-            />
-          </>
-        </ProductList>
-      </main>
-    );
-  }
+	render() {
+		return (
+			<main>
+				<h2>women</h2>
+				<ProductList>
+					<>
+						<ProductCard
+							src="https://source.unsplash.com/random/?dress"
+							title="dress"
+							price="200"
+							currency="$"
+						/>
+						<ProductCard
+							src="https://source.unsplash.com/random/?dress"
+							title="dress"
+							price="200"
+							currency="$"
+						/>
+						<ProductCard
+							src="https://source.unsplash.com/random/?dress"
+							title="dress"
+							price="200"
+							currency="$"
+						/>
+						<ProductCard
+							src="https://source.unsplash.com/random/?dress"
+							title="dress"
+							price="200"
+							currency="$"
+						/>
+					</>
+				</ProductList>
+			</main>
+		);
+	}
 }
 
 export default ProductsListing;
