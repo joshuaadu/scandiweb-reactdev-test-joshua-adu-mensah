5e3b4711a6b2be2c4180f02abd545ab0e6409685
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-07-21 18:28:25 +0000

    add ProductDescription page

--- src/pages/ProductDescription.js
+++ src/pages/ProductDescription.js
@@ -1,8 +1,21 @@
 import { Component } from "react";
+import Sizes from "../components/UI/Sizes";
 
 class ProductDescription extends Component {
 	render() {
-		return;
+		return (
+			<>
+				<section>
+					<div></div>
+					<div></div>
+				</section>
+				<aside>
+					<h2>Apollo</h2>
+					<p>Running Short</p>
+					<Sizes sizes={["S", "M"]} />
+				</aside>
+			</>
+		);
 	}
 }
 
