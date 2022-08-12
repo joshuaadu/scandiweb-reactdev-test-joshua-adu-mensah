0ac503f5d6bc8f88855f24780cf5b28a6ed33976
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-08-12 23:36:15 +0000

    fit ProductCard image to the width of the card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,13 +1,16 @@
 import { Component } from "react";
 import styles from "../styles/ProductCard.module.scss";
 import { ReactComponent as CartIcon } from "../assets/cart-icon-green.svg";
+import { Link } from "react-router-dom";
 
 class ProductCard extends Component {
 	render() {
 		return (
 			<div className={styles.card}>
 				<img src={this.props.src} alt={`${this.props.title} item`} />
-				<p className={styles.title}>{this.props.title}</p>
+				<p className={styles.title}>
+					<Link to="/item">{this.props.title}</Link>
+				</p>
 				<p className={styles.price}>{`${this.props.currency}${Number(
 					this.props.price
 				).toFixed(2)}`}</p>
