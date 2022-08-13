b54defe016613fa84704a569d34c31e81c83ed39
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-08-13 23:57:58 +0000

    add content placeholder to ProductCard

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,17 +4,56 @@ import { ReactComponent as CartIcon } from "../assets/cart-icon-green.svg";
 import { Link } from "react-router-dom";
 
 class ProductCard extends Component {
+	state = {
+		isLoaded: false,
+	};
+
+	componentDidMount() {
+		this.timer = setTimeout(
+			() =>
+				this.setState({
+					isLoaded: true,
+				}),
+			1500
+		);
+	}
+
+	componentWillUnmount() {
+		clearTimeout(this.timer);
+	}
 	render() {
+		const headerClassName = `${styles.header} ${
+			!this.state.isLoaded ? styles["animated-bg"] : ""
+		}`;
+		const textClassName = `${
+			!this.state.isLoaded
+				? styles["animated-bg"] + " " + styles["animated-bg-text"]
+				: ""
+		} `;
 		return (
 			<div className={styles.card}>
-				<img src={this.props.src} alt={`${this.props.title} item`} />
-				<p className={styles.title}>
-					<Link to="/item">{this.props.title}</Link>
-				</p>
-				<p className={styles.price}>{`${this.props.currency}${Number(
-					this.props.price
-				).toFixed(2)}`}</p>
-				<CartIcon className={styles["cart-icon"]} />
+				<div className={headerClassName}>
+					<img
+						style={{
+							visibility: this.state.isLoaded ? "visible" : "hidden",
+						}}
+						src={this.props.src}
+						alt={`${this.props.title} item`}
+					/>
+				</div>
+				<div className={styles.content}>
+					<p className={`${styles.title} ${textClassName}`}>
+						<Link to="/item">
+							{this.state.isLoaded ? this.props.title : "text"}
+						</Link>
+					</p>
+					<p className={`${styles.price} ${textClassName}`}>
+						{this.state.isLoaded
+							? `${this.props.currency}${Number(this.props.price).toFixed(2)}`
+							: "text"}
+					</p>
+					<CartIcon className={`${styles["cart-icon"]} `} />
+				</div>
 			</div>
 		);
 	}
